77ee39c4690731b7e2a634ead46b2426ebb86baa
Bichosiusiu <132112798+Bichosiusiu@users.noreply.github.com>
2025-01-23 12:49:31 -0600

    Delete Sistemas Operativos/Practicas/Unidad2_Practica5 directory

--- Sistemas Operativos/Practicas/Unidad2_Practica5/P7W/bisnietos.c
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
-#include <stdio.h>
-int main(){
-    printf("\t\t\t Proceso bisnieto, Mi PID es: %d\n", GetCurrentProcessId());
-    return 0;
-}
--- Sistemas Operativos/Practicas/Unidad2_Practica5/p2w.c
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-#include <stdio.h>
-int main(void)
-{
- printf("Soy el hijo\n");
- exit(0);
-} 
-
